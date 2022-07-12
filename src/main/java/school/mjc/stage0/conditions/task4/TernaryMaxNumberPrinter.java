d4a57b884e0887e4e801245238c59d34268d12d5
Jakub M <fotograflubin@gmail.com>
2022-07-12 14:36:09 +0200

    done

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int result = first > second ? first : second;
+        int result2 = result > third ? result : third;
+        System.out.println(result2);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryPrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryPrinter {
     public void printWhichIsBigger(int first, int second) {
+        String result = first > second ? "first" : "second";
+
+        //String print = result == first ? "first" : "second";
+
+        System.out.println(result);
     }
 }
